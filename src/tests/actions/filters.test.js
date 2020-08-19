614266447db7d99d6228a195a4e6133eeb074daa
heuzin <matheusqs.arq@gmail.com>
2020-08-18 21:41:28 -0400

    testing for reducers filters and expesnses selectors

---
+++ src/tests/actions/filters.test.js
@@ -0,0 +1,48 @@
+import moment from 'moment'
+import { setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate } from '../../actions/filters';
+
+test("should generate start date action object", () => {
+    const action = setStartDate(moment(0));
+    expect(action).toEqual({
+        type: 'SET_START_DATE',
+        startDate: moment(0)
+    })
+});
+
+test('should generate set end date action object', () => {
+    const action = setEndDate(moment(0));
+    expect(action).toEqual({
+        type: 'SET_END_DATE',
+        endDate: moment(0)
+    })
+});
+
+test('should generate action object for sort by date', () => {
+    const action = sortByDate()
+    expect(action).toEqual({
+        type: 'SORT_BY_DATE'
+    })
+})
+
+test('should generate action object for sort by amount', () => {
+    const action = sortByAmount()
+    expect(action).toEqual({
+        type: 'SORT_BY_AMOUNT'
+    })
+})
+
+test('should generate set text filter object with text value', () => {
+    const action = setTextFilter('testing')
+    expect(action).toEqual({
+        type: 'SET_TEXT_FILTER',
+        text: 'testing'
+    })
+})
+
+test('should generate set text filter object with default', () => {
+    const action = setTextFilter()
+    expect(action).toEqual({
+        type: 'SET_TEXT_FILTER',
+        text: ''
+    })
+})
