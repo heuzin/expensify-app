dec59d8d28d75569cab7650f5b8c5fe75a98228c
heuzin <matheusqs.arq@gmail.com>
2020-08-30 21:31:11 -0400

    deplying issue

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import AppRouter from './routers/AppRouter'
-import configureStore from './store/configureStore'
+import { Provider } from 'react-redux';
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses'
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
@@ -14,12 +14,13 @@ import './firebase/firebase';
 const store = configureStore();
 
 const jsx = (
-    <Provider store={store}>
-        <AppRouter />
-    </Provider>
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
 );
+
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
 store.dispatch(startSetExpenses()).then(() => {
-    ReactDOM.render(jsx, document.getElementById('app'));
+  ReactDOM.render(jsx, document.getElementById('app'));
 });
